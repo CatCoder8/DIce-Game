628c14a5f561f6578a1a52ac5c5023ae74c0b759
CatCoder8 <johncarlo.sarmiento.b@bulsu.edu.ph>
2022-08-25 11:03:51 +0800

    Build roll dice.

--- script.js
+++ script.js
@@ -3,18 +3,45 @@
 // Initialize
 let p1Score = document.querySelector('#score--0');
 let p2Score = document.querySelector('#score--1');  
-
+let p1CurrentScore = document.querySelector('#current--0');  
+let p2CurrentScore = document.querySelector('#current--1');  
 let scoreLabel = document.querySelectorAll('.score');
 let currentScoreLabel = document.querySelectorAll('.current-score');
+let p1CurrentScoreLabel = document.querySelector('#current--0');
+let p2CurrentScoreLabel = document.querySelector('#current--1');
 
 const player = document.querySelector('.player');
+const p0 = document.querySelector('.player--0');
+const p1 = document.querySelector('.player--1');
 
 let dice = document.querySelector('.dice');
-const roll = document.querySelector('.btn--roll');
-const newGame = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
 
 let diceNum;
 let score = 0;
+let currentScore = 0;
+
+const switchPlayer = function() {     
+    if (p0.classList.contains('.player--active')){
+        p0.classList.remove('.player--active')
+        p1.classList.add('.player--active');
+    }
+    else {
+        p1.classList.remove('.player--active')
+        p0.classList.add('.player--active');
+    }       
+}
+
+// FIX classList contains
+
+const currentPlayer = function(){
+    if(p0.classList.contains('.player--active'))
+        return 'p1';
+    else 
+        return 'p0';
+}
 
 // Resetting Game
 const reset = function(){
@@ -28,12 +55,12 @@ const reset = function(){
     dice.classList.add('hidden');
     player.classList.add('player--active');
 }
-reset();
 
-newGame.addEventListener('click', reset)
+reset();
+btnNew.addEventListener('click', reset)
 
 // Roll Dice
-roll.addEventListener('click', function(){
+btnRoll.addEventListener('click', function(){
     
     // Displaying dice
     dice.classList.remove('hidden');
@@ -41,12 +68,42 @@ roll.addEventListener('click', function(){
     // Generating random dice
     diceNum = Math.ceil(Math.random()*6);
     dice.setAttribute('src',`dice-${diceNum}.png`)
-   
+
     // Setting score
     if (diceNum != 1){
-        score += diceNum;
+        if (currentPlayer() === 'p0')  {
+            currentScore += diceNum;
+            p1CurrentScoreLabel.textContent = currentScore;
+        }
+        
+        if (currentPlayer() === 'p1') {
+            currentScore += diceNum;
+            p2CurrentScoreLabel.textContent = currentScore;
+        }
+    }
+
+    if (diceNum === 1){
+        currentScore === 0;
+        currentScoreLabel.textContent === '0';
+        switchPlayer();
     }
 })
+
+// Hold
+// hold.addEventListener('click', function hold(player){
+//     // Checking score
+//     if (currentScore >= 100){
+//         scoreLabel.textContent = scoreLabel;
+//         player.classList.add('player--winner');
+//     }
+//     else
+//         switchPlayer();
+    
+// })
+
+
+
+
 
 
 
